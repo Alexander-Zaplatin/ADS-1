c3041a88a20746740e77fb2518f6416e97804e06
Alexander-Zaplatin <79161134+Alexander-Zaplatin@users.noreply.github.com>
2021-03-25 01:59:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,21 +3,19 @@
 int cbinsearch(int *arr, int size, int value) {
 int r = size -1;
 int l;
-int s;
-int r = -1;
+int s = -1;
+int e = 0;
 int mid;
 while (l <= r) {
 mid = (l + r) / 2;
 if (arr[mid] == value) s = mid;
-if (value < arr[mid]) r = mid - 1;
-else
-l = mid + 1;
-}
+if (value <= arr[mid]) r = mid - 1;
+if (value > arr[mid]) l = mid + 1;
 }
 if (arr[s] == value) {
 while (arr[s] == value) {
-m++;
 s++;
+e++;
 }
 }
   return 0; // если ничего не найдено
